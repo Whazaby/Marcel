429726f8843b20a6b9a41728d162c431e16a006e
Whazaby <guillaume.ruscassie@gmail.com>
2018-04-13 08:15:38 +0200

    add litige form page

--- src/routes.js
+++ src/routes.js
@@ -50,4 +50,45 @@ router.post('/contact',upload.single('photo'), [
   res.redirect('/')
 })
 
+router.get('/litige', (req, res) => {
+  res.render('litige', {
+    data: {},
+    errors: {},
+    csrfToken: req.csrfToken()
+  })
+})
+
+router.post('/litige',upload.single('photo'), [
+  check('message')
+    .isLength({ min: 1 })
+    .withMessage('Message is required')
+    .trim(),
+  check('email')
+    .isEmail()
+    .withMessage('That email doesn‘t look right')
+    .trim()
+    .normalizeEmail()
+], (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.render('litige', {
+      data: req.body,
+      errors: errors.mapped(),
+      csrfToken: req.csrfToken()
+    })
+  }
+
+  const data = matchedData(req)
+  console.log('Sanitized:', data)
+    
+  if (req.file) {
+    console.log('Uploaded: ', req.file)
+    // Homework: Upload file to S3
+  }
+    
+  req.flash('success', 'Thanks for the message! I‘ll be in touch :)')
+  res.redirect('/')
+})
+
+
 module.exports = router
